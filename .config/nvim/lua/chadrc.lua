58dbad90d96166fee099553f08c49731ebc3f864
mauwQ <mauw0@protonmail.com>
2024-07-01 16:22:15 -0300

    fixed broken NvChad config

---
+++ .config/nvim/lua/chadrc.lua
@@ -0,0 +1,18 @@
+---@type ChadrcConfig
+local M = {}
+
+M.ui = {
+	theme = "tokyodark",
+  transparency = true,
+  statusline = {
+    separator_style = "arrow",
+    theme = "vscode",
+  },
+
+	-- hl_override = {
+	-- 	Comment = { italic = true },
+	-- 	["@comment"] = { italic = true },
+	-- },
+}
+
+return M
