1deedc8c8c08030967ccf879aec4d1a203c5b4c9
mauwQ <mauw0@protonmail.com>
2024-07-23 09:33:46 -0300

    Update config.def.h

--- suckless/dmenu/config.def.h
+++ suckless/dmenu/config.def.h
@@ -4,14 +4,14 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"terminus:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#7a49a5" },
-	[SchemeOut] = { "#000000", "#49A57A" },
+	[SchemeNorm] = { "#6e5e89", "#000000" },
+	[SchemeSel] = { "#6e5e89", "#352b43" },
+	[SchemeOut] = { "#000000", "#222222" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
