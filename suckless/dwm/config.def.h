70ac79bee227a6683e024b5d0747988b68ba1314
mauwQ <mauw0@protonmail.com>
2024-07-23 00:28:30 -0300

    Update config.def.h

--- suckless/dwm/config.def.h
+++ suckless/dwm/config.def.h
@@ -13,24 +13,19 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "terminus:size=12" };
 static const char dmenufont[]       = "terminus:size=12";
-//static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-//static const char col_gray4[]       = "#eeeeee";
-//static const char col_cyan[]        = "#005577";
 
-/* my colors */
-static const char col_dark_gray[]   = "#222222";
-static const char col_white[]       = "#eeeeee";
-static const char col_violet[]      = "#7a49a5";
-static const char col_green[]       = "#49A57A";
+static const char col_gray1[]       = "#000000";
+static const char col_gray2[]       = "#181321";
+static const char col_gray3[]       = "#6e5e89";
+static const char col_gray4[]       = "#6e5e89";
+static const char col_cyan[]        = "#352b43";
 
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_dark_gray, col_gray2 },
-	[SchemeSel]  = { col_white, col_violet,  col_violet  },
-	[SchemeHov]  = { col_white, col_green,  col_violet  },
-	[SchemeHid]  = { col_violet,col_dark_gray, col_violet  },
+  [SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+  [SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+  [SchemeHov]  = { col_gray4, col_cyan,  col_cyan  },	
+  [SchemeHid]  = { col_cyan,  col_gray1, col_cyan  },
 };
 
 /* tagging */
@@ -72,7 +67,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_dark_gray, "-nf", col_gray3, "-sb", col_violet, "-sf", col_white, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *webbrowser[] = { "chromium", NULL };
 static const char *filemanager[] = { "pcmanfm", NULL };
